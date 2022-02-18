7e057e73e870c095c506caf3d08dfdcb39741cc7
iamppz <iamppz@hotmail.com>
2022-02-18 20:24:22 +0800

    fix: update to 2.2.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Flowchart from "flowchart-react";
+import { ConnectionData, NodeData } from "flowchart-react/dist/schema";
 
 function App() {
+  const [nodes, setNodes] = useState<NodeData[]>([
+    {
+      type: "start",
+      name: "Start",
+      x: 150,
+      y: 190,
+      id: 1604410569920,
+      approvers: [],
+    },
+    {
+      type: "end",
+      name: "End",
+      x: 500,
+      y: 190,
+      id: 1604410572363,
+      approvers: [],
+    },
+    {
+      x: 330,
+      y: 190,
+      id: 1604410575428,
+      name: "New",
+      type: "operation",
+      approvers: [{ name: "Joyce", id: 1 }],
+    },
+    {
+      x: 330,
+      y: 300,
+      id: 1604410591865,
+      name: "New",
+      type: "operation",
+      approvers: [],
+    },
+  ]);
+
+  const [conns, setConns] = useState<ConnectionData[]>([
+    {
+      source: { id: 1604410569920, position: "right" },
+      destination: { id: 1604410575428, position: "left" },
+      id: 1604410587907,
+      type: "pass",
+    },
+    {
+      source: { id: 1604410575428, position: "right" },
+      destination: { id: 1604410572363, position: "left" },
+      id: 1604410590524,
+      type: "pass",
+    },
+    {
+      source: { id: 1604410569920, position: "bottom" },
+      destination: { id: 1604410591865, position: "left" },
+      id: 1604410596866,
+      type: "pass",
+    },
+    {
+      source: { id: 1604410591865, position: "right" },
+      destination: { id: 1604410572363, position: "bottom" },
+      id: 1604410599205,
+      type: "pass",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Flowchart
+        onChange={(nodes, connections) => {
+          setNodes(nodes);
+          setConns(connections);
+        }}
+        style={{ width: 640, height: 480 }}
+        nodes={nodes}
+        connections={conns}
+      />
     </div>
   );
 }

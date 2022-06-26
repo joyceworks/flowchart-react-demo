a258db4c6de482f69e3b6d4bd3796db59ad019a6
iamppz <iamppz@hotmail.com>
2022-06-26 14:55:28 +0800

    feat(decision): add

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Flowchart from "flowchart-react";
-import { ConnectionData, NodeData } from "flowchart-react/dist/schema";
+import {ConnectionData, NodeData} from "flowchart-react/dist/schema";
 
 const App = () => {
   const [nodes, setNodes] = useState<NodeData[]>([
     {
       type: "start",
       title: "Start",
-      x: 150,
-      y: 190,
+      x: 50,
+      y: 240,
       id: 1604410569920,
     },
     {
       type: "end",
       title: "End",
-      x: 500,
-      y: 190,
+      x: 570,
+      y: 240,
       id: 1604410572363,
     },
     {
-      x: 330,
-      y: 190,
+      x: 400,
+      y: 240,
       id: 1604410575428,
       title: "Joyce",
       type: "operation",
     },
     {
-      x: 330,
-      y: 300,
+      x: 230,
+      y: 240,
       id: 1604410591865,
       title: () => {
-        return "No approver";
+        return "Is leader";
       },
-      type: "operation",
+      type: "decision",
     },
   ]);
   const [conns, setConns] = useState<ConnectionData[]>([
     {
-      source: { id: 1604410569920, position: "right" },
-      destination: { id: 1604410575428, position: "left" },
+      source: {id: 1604410569920, position: "right"},
+      destination: {id: 1604410591865, position: "left"},
       id: 1604410587907,
       type: "success",
     },
     {
-      source: { id: 1604410575428, position: "right" },
-      destination: { id: 1604410572363, position: "left" },
+      source: {id: 1604410575428, position: "right"},
+      destination: {id: 1604410572363, position: "left"},
       id: 1604410590524,
       type: "success",
     },
     {
-      source: { id: 1604410569920, position: "bottom" },
-      destination: { id: 1604410591865, position: "left" },
-      id: 1604410596866,
-      type: "success",
+      source: {id: 1604410591865, position: "right"},
+      destination: {id: 1604410575428, position: "left"},
+      id: 1604410590524,
+      type: "fail",
     },
     {
-      source: { id: 1604410591865, position: "right" },
-      destination: { id: 1604410572363, position: "bottom" },
-      id: 1604410599205,
+      source: {id: 1604410591865, position: "bottom"},
+      destination: {id: 1604410572363, position: "bottom"},
+      id: 1604410590524,
       type: "success",
     },
   ]);
@@ -68,9 +68,10 @@ const App = () => {
         setNodes(nodes);
         setConns(connections);
       }}
-      style={{ width: 800, height: 600 }}
+      style={{width: 800, height: 600}}
       nodes={nodes}
       connections={conns}
+      showToolbar={true}
     />
   );
 };
